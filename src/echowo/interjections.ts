852ad6540b9d32e117680da7bd398d72c1dc81ef
Markus Aigner <perceptified@gmail.com>
2023-05-16 23:44:50 +0200

    added stuff

--- src/echowo/interjections.ts
+++ src/echowo/interjections.ts
@@ -1,22 +1,37 @@
 import { randomGenerator } from "./random-generator"
 import { uwuInterjections } from "../data/interjections.json"
 export class Interjections {
-    public static insertInterjections(inputString : string) {
+    /**
+     * @description Takes the input string and a number of interjections to insert (in guaranteed mode, one at least)
+     * @param inputString : string string to be outfitted with interjections
+     * @returns the inputString with interjections
+     */
+    public static insertInterjections(inputString : string, guaranteed : boolean) {
         let inputCharArray = Array.from(inputString)
         let spaceIndices = []
+        let interjectionIndices = []
+        let numberOfInterjections : number
         for(let counter = 0; counter < inputString.length; counter = counter + 1) {
             if(inputString[counter] === " ") {
                 spaceIndices.push(counter)
             }
         }
-        for(let counter = 0; counter < spaceIndices.length; counter = counter + 1) {
+        if(guaranteed === true) {
+            numberOfInterjections = randomGenerator.getRandomNumber(1, spaceIndices.length)
+        }
+        else {
+            numberOfInterjections = randomGenerator.getRandomNumber(0, spaceIndices.length)
+        }
+        for(let counter = 0; counter < numberOfInterjections; counter = counter + 1) {
+            let number = randomGenerator.getRandomNumber(0, spaceIndices.length)
+            interjectionIndices.push(number)
+        }
+        // console.log(interjectionIndices.toString())
+        for(let counter = 0; counter < interjectionIndices.length; counter = counter + 1) {
             let number = randomGenerator.getRandomNumber(0, uwuInterjections.length)
             inputCharArray[spaceIndices[counter]] = " " + uwuInterjections[number] + " "
         }
         inputString = inputCharArray.join("")
-        // console.log(spaceIndices.toString())
-        // console.log(spaceIndices.toString())
-        return inputString
+        return interjectionIndices.toString()
     }
-    
 }
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ program.version(helpInstance.getProgramVersion())
 optionInstance.getOptions().forEach((value : string, key : string) => {
     program.option(key, value)
 })
+let testString = "Lola met Mark at the mall. She noticed his bulge, cracking her neck. He was really tall. They walked and talked, exploring every store. Lola's eyes sparkled with delight as they found a plush, purple pillow. Mark couldn't help but smile, knowing he had found the perfect gift."
 // Replacements.uwuTransform("Lola met Mark at the mall. She noticed his bulge, cracking her neck. He was really tall. They walked and talked, exploring every store. Lola's eyes sparkled with delight as they found a plush, purple pillow. Mark couldn't help but smile, knowing he had found the perfect gift.")
-console.log(Interjections.insertInterjections("Lola met Mark at the mall. She noticed his bulge, cracking her neck. He was really tall. They walked and talked, exploring every store. Lola's eyes sparkled with delight as they found a plush, purple pillow. Mark couldn't help but smile, knowing he had found the perfect gift."))
+console.log(Interjections.insertInterjections(testString, true))
 program.parse()
